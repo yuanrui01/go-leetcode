043a3c6ec87aca91696f4d78d591a19251fa6b02
yuanrui01 <xdyrfree@gmail.com>
2024-09-28 00:13:12 +0800

    739. 每日温度

---
+++ daily/dailyTemperatures.go
@@ -0,0 +1,17 @@
+package daily
+
+// 739. 每日温度
+func dailyTemperatures(temperatures []int) []int {
+	n := len(temperatures)
+	ans := make([]int, n)
+	st := []int{}
+	for i, t := range temperatures {
+		for len(st) > 0 && temperatures[st[len(st)-1]] < t {
+			j := st[len(st)-1]
+			st = st[:len(st)-1]
+			ans[j] = i - j
+		}
+		st = append(st, i)
+	}
+	return ans
+}
