a424924556ead35c6ebe7709e9887457d292c81f
yuanrui <xdyrfree@gmail.com>
2024-10-25 15:33:57 +0800

    746. 使用最小花费爬楼梯

--- dp/minCostClimbingStairs.go
+++ dp/minCostClimbingStairs.go
@@ -3,9 +3,11 @@ package dp
 // 746. 使用最小花费爬楼梯
 func minCostClimbingStairs(cost []int) int {
 	n := len(cost)
-	dp := make([]int, n+1)
+	f0, f1 := 0, 0
 	for i := 2; i <= n; i++ {
-		dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+		t := f1
+		f1 = min(f0+cost[i-2], f1+cost[i-1])
+		f0 = t
 	}
-	return dp[n]
+	return f1
 }
