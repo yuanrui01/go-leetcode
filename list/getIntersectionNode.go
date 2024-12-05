97c59156932b16ac80745114f7606ac2b14a6090
yuanrui01 <xdyrfree@gmail.com>
2024-12-06 06:41:30 +0800

    160. 相交链表

---
+++ list/getIntersectionNode.go
@@ -0,0 +1,19 @@
+package main
+
+// 160. 相交链表
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	pA, pB := headA, headB
+	for pA != pB {
+		if pA == nil {
+			pA = headB
+		} else {
+			pA = pA.Next
+		}
+		if pB == nil {
+			pB = headA
+		} else {
+			pB = pB.Next
+		}
+	}
+	return pA
+}
