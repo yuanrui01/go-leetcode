c928521884a63651d8ada2a9945462b1b9f0269d
yuanrui <xdyrfree@gmail.com>
2025-01-12 09:45:08 +0800

    2275. 按位与结果大于零的最长组合

--- daily/largestCombination.go
+++ daily/largestCombination.go
@@ -1,20 +1,34 @@
 package daily
 
+import "slices"
+
 // 2275. 按位与结果大于零的最长组合
 func largestCombination(candidates []int) int {
-	cnt := make([]int, 24)
-	ans := 0
+	cnt := [24]int{}
 	for _, v := range candidates {
-		k := 0
 		num := v
-		for num != 0 {
-			cnt[k] += num & 1
-			k++
+		for i := 0; num != 0; i++ {
+			cnt[i] += num & 1
 			num >>= 1
 		}
 	}
-	for _, v := range cnt {
-		ans = max(ans, v)
-	}
-	return ans
+	return slices.Max(cnt[:])
 }
+
+//func largestCombination_bak(candidates []int) int {
+//	cnt := make([]int, 24)
+//	ans := 0
+//	for _, v := range candidates {
+//		k := 0
+//		num := v
+//		for num != 0 {
+//			cnt[k] += num & 1
+//			k++
+//			num >>= 1
+//		}
+//	}
+//	for _, v := range cnt {
+//		ans = max(ans, v)
+//	}
+//	return ans
+//}
