708b3c59df1b2e17fea8650ff47674e263da8b5b
yuanrui <xdyrfree@gmail.com>
2025-01-14 21:25:49 +0800

    3066. 超过阈值的最少操作数 II

--- daily/minOperations3066.go
+++ daily/minOperations3066.go
@@ -2,11 +2,12 @@ package daily
 
 import (
 	"container/heap"
+	"sort"
 )
 
 // 3066. 超过阈值的最少操作数 II
 func minOperations3066(nums []int, k int) (ans int) {
-	h := &hp{nums}
+	h := &mh{nums}
 	heap.Init(h)
 	for h.IntSlice[0] < k {
 		x := heap.Pop(h).(int)
@@ -17,13 +18,12 @@ func minOperations3066(nums []int, k int) (ans int) {
 	return ans
 }
 
-//
-//type hp struct{ sort.IntSlice }
-//
-//func (hp) Push(any) {}
-//func (h *hp) Pop() any {
-//	a := h.IntSlice
-//	v := a[len(a)-1]
-//	h.IntSlice = a[:len(a)-1]
-//	return v
-//}
+type mh struct{ sort.IntSlice }
+
+func (mh) Push(any) {}
+func (h *mh) Pop() any {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
