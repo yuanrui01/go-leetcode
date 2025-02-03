f2d1aa4e7b5e402f40ad6430569f8554b87b0068
Ovo?? <xdyrfree@gmail.com>
2025-02-03 10:39:02 +0800

    680. Valid Palindrome II

---
+++ daily/validPalindrome.go
@@ -0,0 +1,23 @@
+// 680. Valid Palindrome II
+func validPalindrome(s string) bool {
+	var valid func(s string, left int, right int) bool
+	valid = func(s string, left int, right int) bool {
+		for left < right {
+			if s[left] != s[right] {
+				return false
+			}
+			left++
+			right--
+		}
+		return true
+	}
+	left, right := 0, len(s)-1
+	for left < right {
+		if s[left] != s[right] {
+			return valid(s, left+1, right) || valid(s, left, right-1)
+		}
+		left++
+		right--
+	}
+	return true
+}
