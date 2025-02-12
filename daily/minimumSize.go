09b163f7f6412c1f7fec8cf1b1bab4c59f0991db
yuanrui <xdyrfree@gmail.com>
2025-02-12 13:30:13 +0800

    库函数

--- daily/minimumSize.go
+++ daily/minimumSize.go
@@ -1,25 +1,19 @@
 package daily
 
-import "slices"
+import (
+	"slices"
+	"sort"
+)
 
 // 1760. 袋子里最少数目的球
 func minimumSize(nums []int, maxOperations int) int {
 	left, right := 1, slices.Max(nums)
-	var check func(int) bool
-	check = func(m int) bool {
+	return left + sort.Search(right-left, func(m int) bool {
+		m += left
 		cnt := 0
 		for _, num := range nums {
 			cnt += (num - 1) / m
 		}
 		return cnt <= maxOperations
-	}
-	for left <= right {
-		mid := left + (right-left)/2
-		if check(mid) {
-			right = mid - 1
-		} else {
-			left = mid + 1
-		}
-	}
-	return left
+	})
 }
