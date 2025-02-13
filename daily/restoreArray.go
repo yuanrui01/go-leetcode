018528154418a117e76aac3f5250082d948c65e9
yuanrui <xdyrfree@gmail.com>
2025-02-13 10:00:03 +0800

    1743. 从相邻元素对还原数组

--- daily/restoreArray.go
+++ daily/restoreArray.go
@@ -3,19 +3,18 @@ package daily
 // 1743. 从相邻元素对还原数组
 func restoreArray(adjacentPairs [][]int) []int {
 	n := len(adjacentPairs)
-	vis := make(map[int]bool)
 	ans := make([]int, n+1)
-	adj := make(map[int][]int, n)
+	adjMap := make(map[int][]int, n)
 
 	// 构建邻接表
 	for _, pair := range adjacentPairs {
-		adj[pair[0]] = append(adj[pair[0]], pair[1])
-		adj[pair[1]] = append(adj[pair[1]], pair[0])
+		adjMap[pair[0]] = append(adjMap[pair[0]], pair[1])
+		adjMap[pair[1]] = append(adjMap[pair[1]], pair[0])
 	}
 
 	// 找到起始点
 	var start int
-	for k, v := range adj {
+	for k, v := range adjMap {
 		if len(v) == 1 {
 			start = k
 			break
@@ -24,11 +23,11 @@ func restoreArray(adjacentPairs [][]int) []int {
 
 	// 构造结果
 	ans[0] = start
-	vis[start] = true
 	for i := 0; i < n; i++ {
-		for _, v := range adj[ans[i]] {
-			if !vis[v] {
-				vis[v] = true
+		adj := adjMap[ans[i]]
+		delete(adjMap, ans[i])
+		for _, v := range adj {
+			if adjMap[v] != nil {
 				ans[i+1] = v
 				break
 			}
