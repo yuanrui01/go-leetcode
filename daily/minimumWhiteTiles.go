d405c7126a8f1011ad635e2d6741bc2364f069e7
yuanrui <xdyrfree@gmail.com>
2025-02-21 15:22:27 +0800

    2209. 用地毯覆盖后的最少白色砖块

--- daily/minimumWhiteTiles.go
+++ daily/minimumWhiteTiles.go
@@ -3,28 +3,18 @@ package daily
 // 2209. 用地毯覆盖后的最少白色砖块
 func minimumWhiteTiles(floor string, numCarpets int, carpetLen int) int {
 	m := len(floor)
-	memo := make([][]int, numCarpets+1)
-	for i := range memo {
-		memo[i] = make([]int, m)
-		for j := range memo[i] {
-			memo[i][j] = -1
+	dp := make([][]int, numCarpets+1)
+	for i := range dp {
+		dp[i] = make([]int, m)
+	}
+	dp[0][0] = int(floor[0] - '0')
+	for i := 1; i < m; i++ {
+		dp[0][i] = dp[0][i-1] + int(floor[i]-'0')
+	}
+	for i := 1; i <= numCarpets; i++ {
+		for j := i * carpetLen; j < m; j++ {
+			dp[i][j] = min(dp[i-1][j-carpetLen], dp[i][j-1]+int(floor[j]-'0'))
 		}
 	}
-	var dfs func(int, int) int
-	dfs = func(i, j int) (res int) {
-		if j < carpetLen*i {
-			return
-		}
-		p := &memo[i][j]
-		if *p != -1 {
-			return *p
-		}
-		defer func() { *p = res }()
-		res = dfs(i, j-1) + int(floor[j]-'0')
-		if i > 0 {
-			res = min(res, dfs(i-1, j-carpetLen))
-		}
-		return
-	}
-	return dfs(numCarpets, m-1)
+	return dp[numCarpets][m-1]
 }
