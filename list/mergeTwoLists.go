47d036b39063cd8afdf3c2410a2b00861bb911cd
yuanrui <xdyrfree@gmail.com>
2025-04-09 14:17:37 +0800

    876. 链表的中间结点

--- list/mergeTwoLists.go
+++ list/mergeTwoLists.go
@@ -1,3 +1,5 @@
+package main
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -6,26 +8,26 @@
  * }
  */
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-    head := &ListNode{}
-    cur := head
-    for list1 != nil && list2 != nil {
-        if list1.Val < list2.Val {
-            cur.Next = list1
-            list1 = list1.Next
-        } else {
-            cur.Next = list2
-            list2 = list2.Next
-        }
-        cur = cur.Next
-    }
+	head := &ListNode{}
+	cur := head
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		cur = cur.Next
+	}
 
-    if list1 != nil {
-        cur.Next = list1
-    }
+	if list1 != nil {
+		cur.Next = list1
+	}
 
-    if list2 != nil {
-        cur.Next = list2
-    }
+	if list2 != nil {
+		cur.Next = list2
+	}
 
-    return head.Next
+	return head.Next
 }
