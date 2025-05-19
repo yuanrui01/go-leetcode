45eb5d94e31f598f8f5ea01f7be00f651b10fe2a
yuanrui <xdyrfree@gmail.com>
2025-05-19 13:22:43 +0800

    3024. 三角形类型

---
+++ daily/triangleType.go
@@ -0,0 +1,19 @@
+package daily
+
+import "slices"
+
+// 3024. 三角形类型
+func triangleType(nums []int) string {
+	slices.Sort(nums)
+	a, b, c := nums[0], nums[1], nums[2]
+	if a+b <= c {
+		return "none"
+	}
+	if a == c {
+		return "equilateral"
+	}
+	if a == b || b == c {
+		return "isosceles"
+	}
+	return "scalene"
+}
